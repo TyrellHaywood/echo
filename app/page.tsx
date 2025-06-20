68504493da35be40495ea5cc0c0ae480e426f255
TyrellHaywood <tyrelltman@gmail.com>
2025-06-20 01:09:13 -0400

    feat: simple homepage UI skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
+// Shadcn components
+import { Menubar, MenubarMenu } from "@/components/ui/menubar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Logo } from "@/components/ui/logo";
+
+// Icons
+import { Waypoints, Plus, User } from "lucide-react";
+
 export default function Home() {
   return (
     <div>
-      <h1></h1>
+      {/* Top menu */}
+      <div className="flex flex-row gap-4">
+        <Logo className="absolute top-4 left-4" />
+        <div className="flex flex-row gap-4 items-center absolute top-4 left-1/2 transform -translate-x-1/2 w-1/3">
+          <Input type="text" placeholder="Search"></Input>
+        </div>
+      </div>
+
+      {/* Toolbar */}
+      <Menubar className="absolute bottom-4 left-1/2 transform -translate-x-1/2 p-0 space-x-0">
+        <MenubarMenu>
+          <Button variant="ghost" className="hover:border hover:border-input">
+            <Waypoints />
+          </Button>
+          <Button variant="ghost" className="hover:border hover:border-input">
+            <Plus />
+          </Button>
+          <Button variant="ghost" className="hover:border hover:border-input">
+            <User />
+          </Button>
+        </MenubarMenu>
+      </Menubar>
     </div>
   );
 }
