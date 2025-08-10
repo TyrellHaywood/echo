37f4c8dd48e6de80e1883973fb16fc1720f39915
TyrellHaywood <tyrelltman@gmail.com>
2025-08-10 17:13:01 -0400

    #37 style: udpated style for graphSkeleton

--- components/metaGraph/GraphSkeleton.tsx
+++ components/metaGraph/GraphSkeleton.tsx
@@ -3,90 +3,33 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function GraphSkeleton() {
   return (
     <div className="w-full h-screen relative bg-background">
-      {/* Simulated graph nodes scattered across the screen */}
       <div className="absolute inset-0">
-        {/* Central cluster */}
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <Skeleton className="w-8 h-8 rounded-md" />
-          <Skeleton className="w-12 h-3 mt-2 mx-auto" />
-        </div>
+        {/* Central node */}
+        <Skeleton className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-7 h-7 rounded-md" />
 
         {/* Top left cluster */}
-        <div className="absolute top-1/4 left-1/4">
-          <Skeleton className="w-6 h-6 rounded-md" />
-          <Skeleton className="w-8 h-2 mt-1 mx-auto" />
-        </div>
-
-        <div className="absolute top-1/3 left-1/5">
-          <Skeleton className="w-7 h-7 rounded-md" />
-          <Skeleton className="w-10 h-2 mt-1 mx-auto" />
-        </div>
+        <Skeleton className="absolute top-1/4 left-1/4 w-6 h-6 rounded-md" />
+        <Skeleton className="absolute top-1/3 left-1/5 w-6 h-6 rounded-md" />
 
         {/* Top right cluster */}
-        <div className="absolute top-1/4 right-1/4">
-          <Skeleton className="w-6 h-6 rounded-md" />
-          <Skeleton className="w-9 h-2 mt-1 mx-auto" />
-        </div>
-
-        <div className="absolute top-1/3 right-1/5">
-          <Skeleton className="w-7 h-7 rounded-md" />
-          <Skeleton className="w-8 h-2 mt-1 mx-auto" />
-        </div>
+        <Skeleton className="absolute top-1/4 right-1/4 w-6 h-6 rounded-md" />
+        <Skeleton className="absolute top-1/3 right-1/5 w-6 h-6 rounded-md" />
 
         {/* Bottom left cluster */}
-        <div className="absolute bottom-1/4 left-1/4">
-          <Skeleton className="w-6 h-6 rounded-md" />
-          <Skeleton className="w-7 h-2 mt-1 mx-auto" />
-        </div>
-
-        <div className="absolute bottom-1/3 left-1/5">
-          <Skeleton className="w-8 h-8 rounded-md" />
-          <Skeleton className="w-10 h-2 mt-1 mx-auto" />
-        </div>
+        <Skeleton className="absolute bottom-1/4 left-1/4 w-6 h-6 rounded-md" />
+        <Skeleton className="absolute bottom-1/3 left-1/5 w-7 h-7 rounded-md" />
 
         {/* Bottom right cluster */}
-        <div className="absolute bottom-1/4 right-1/4">
-          <Skeleton className="w-7 h-7 rounded-md" />
-          <Skeleton className="w-9 h-2 mt-1 mx-auto" />
-        </div>
-
-        <div className="absolute bottom-1/3 right-1/5">
-          <Skeleton className="w-6 h-6 rounded-md" />
-          <Skeleton className="w-8 h-2 mt-1 mx-auto" />
-        </div>
+        <Skeleton className="absolute bottom-1/4 right-1/4 w-6 h-6 rounded-md" />
+        <Skeleton className="absolute bottom-1/3 right-1/5 w-6 h-6 rounded-md" />
 
         {/* Additional scattered nodes */}
-        <div className="absolute top-2/3 left-1/3">
-          <Skeleton className="w-5 h-5 rounded-md" />
-          <Skeleton className="w-6 h-2 mt-1 mx-auto" />
-        </div>
-
-        <div className="absolute top-1/6 left-2/3">
-          <Skeleton className="w-6 h-6 rounded-md" />
-          <Skeleton className="w-8 h-2 mt-1 mx-auto" />
-        </div>
-
-        <div className="absolute bottom-1/6 left-2/3">
-          <Skeleton className="w-7 h-7 rounded-md" />
-          <Skeleton className="w-9 h-2 mt-1 mx-auto" />
-        </div>
-
-        <div className="absolute top-2/3 right-1/3">
-          <Skeleton className="w-6 h-6 rounded-md" />
-          <Skeleton className="w-7 h-2 mt-1 mx-auto" />
-        </div>
-
-        {/* Simulated connection lines */}
-        <div className="absolute top-1/2 left-1/2 w-24 h-px bg-primary/10 transform -translate-x-1/2 -translate-y-1/2 rotate-45" />
-        <div className="absolute top-1/2 left-1/2 w-20 h-px bg-primary/10 transform -translate-x-1/2 -translate-y-1/2 -rotate-45" />
-        <div className="absolute top-1/2 left-1/2 w-16 h-px bg-primary/10 transform -translate-x-1/2 -translate-y-1/2 rotate-12" />
-        <div className="absolute top-1/2 left-1/2 w-18 h-px bg-primary/10 transform -translate-x-1/2 -translate-y-1/2 -rotate-12" />
-      </div>
-
-      {/* Loading text */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex items-center gap-2">
-        <div className="w-4 h-4 border-2 border-primary/30 border-t-primary rounded-full animate-spin" />
-        <span className="text-sm text-muted-foreground">Loading graph...</span>
+        <Skeleton className="absolute top-2/3 left-1/3 w-5 h-5 rounded-md" />
+        <Skeleton className="absolute top-1/6 left-2/3 w-6 h-6 rounded-md" />
+        <Skeleton className="absolute bottom-1/6 left-2/3 w-6 h-6 rounded-md" />
+        <Skeleton className="absolute top-2/3 right-1/3 w-6 h-6 rounded-md" />
+        <Skeleton className="absolute top-1/2 left-1/6 w-5 h-5 rounded-md" />
+        <Skeleton className="absolute top-1/2 right-1/6 w-6 h-6 rounded-md" />
       </div>
     </div>
   );
