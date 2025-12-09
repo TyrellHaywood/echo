6e07677d2f441646047d0405570853450873a907
Tyrell H <73722828+TyrellHaywood@users.noreply.github.com>
2025-12-08 19:10:42 -0500

    test: remove pwa

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   turbopack: {},
@@ -15,9 +14,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA({
-  dest: 'public/sw',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})(nextConfig);
+export default nextConfig;
